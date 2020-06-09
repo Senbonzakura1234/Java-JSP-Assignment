4517ec4bbc39373730705bef74a64da05cf4802a
Senbonzakura <anhdungpham090@gmail.com>
2020-06-10 00:57:37 +0700

    add index servlet for testing template.

---
+++ src/main/java/com/app/controller/IndexServlet.java
@@ -0,0 +1,23 @@
+package com.app.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class IndexServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doProcess(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doProcess(request, response);
+    }
+
+    protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("view/index.jsp").forward(request, response);
+    }
+}
