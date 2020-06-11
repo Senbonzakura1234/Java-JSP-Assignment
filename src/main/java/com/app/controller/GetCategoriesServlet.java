9e73b2760eea1ba5f9606fa2a5ab91e0be2573a7
Senbonzakura <anhdungpham090@gmail.com>
2020-06-11 17:35:56 +0700

    create category servlet.

--- src/main/java/com/app/controller/GetCategoriesServlet.java
+++ src/main/java/com/app/controller/GetCategoriesServlet.java
@@ -1,19 +1,38 @@
 package com.app.controller;
 
+import com.app.context.ProductBean;
+import com.app.entity.Product;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "CategoryServlet")
-public class CategoryServlet extends HttpServlet {
+@WebServlet(name = "CategoryServlet", urlPatterns = "/category")
+public class GetCategoriesServlet extends HttpServlet {
+    @EJB
+    ProductBean productBean;
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+        doProcess(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        if(id == null){
 
+        }
+        List<Product> products = productBean.getProductsOfCategory(id);
+        doProcess(request, response);
     }
+
+
+    protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("view/category/index.jsp").forward(request, response);
+    }
+
+
 }
